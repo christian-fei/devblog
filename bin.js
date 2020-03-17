6f1a4eba546c305be904e28503379877e76ead75
Christian Fei <crifei93@gmail.com>
2020-03-18 00:40:13 +0100

    bin

--- bin.js
+++ bin.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-main()
+const { scan, build } = require('.')
+
+main(process.argv[2] || process.cwd())
   .then(() => process.exit(0))
   .catch(err => console.error(err.message, err) && process.exit(1))
 
-async function main () {
-
+async function main (pathParam) {
+  console.log('pathParam', pathParam)
+  const scanResult = await scan(pathParam)
+  console.log('scanResult', scanResult)
+  const buildResult = await build(scanResult.absoluteBasedir, scanResult.files)
+  console.log('buildResult', buildResult)
 }
