0ad8f0b48c6f6be90851a2bf94faf48b341dbcba
Christian Fei <crifei93@gmail.com>
2020-03-18 21:30:15 +0100

    markdown-file write test

--- test/markdown-file.test.js
+++ test/markdown-file.test.js
@@ -12,3 +12,12 @@ test('reads file information', async t => {
   t.snapshot(result.html)
   t.snapshot(result.md)
 })
+
+test('write output file', async t => {
+  const filepath = path.resolve(__dirname, 'fixtures', 'test.md')
+  const absoluteBasedir = path.resolve(__dirname, 'fixtures')
+  const file = new MarkdownFile(filepath, absoluteBasedir)
+
+  const result = file.write()
+  t.truthy(result.includes('/test/fixtures/_site/test.html'), result)
+})
