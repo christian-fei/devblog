75111cc00d6cd70d0b736a808602459f6e003715
Christian Fei <crifei93@gmail.com>
2020-03-18 20:16:20 +0100

    cleanup bin, fix html in template

--- bin.js
+++ bin.js
@@ -7,9 +7,18 @@ main(process.argv[2] || process.cwd())
   .catch(err => console.error(err.message, err) && process.exit(1))
 
 async function main (pathParam) {
-  console.log('pathParam', pathParam)
-  const scanResult = await scan(pathParam)
-  console.log('scanResult', scanResult)
-  const buildResult = await build(scanResult.absoluteBasedir, scanResult.files)
-  console.log('buildResult', buildResult)
+  const { absoluteBasedir, files } = await scan(pathParam)
+  console.log(`${files.length} files found`)
+  console.log(`processing files..`)
+
+  const { errors, written } = await build(absoluteBasedir, files)
+  if (errors.length > 0) {
+    console.error(`errors: `)
+    console.error(errors.map(e => `"${e.message}" on "${e.filepath}"`).join('\n'))
+  }
+  if (written.length === 0) {
+    console.info('⚠️ no files created')
+  } else {
+    console.log(written.map(w => `created ${w}`).join('\n'))
+  }
 }
