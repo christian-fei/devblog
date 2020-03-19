bb7862e2fa6a5a1a01905982ffcc3bc6b31b6561
Christian Fei <crifei93@gmail.com>
2020-03-19 21:59:48 +0100

    parse collections

---
+++ test/fixtures/test-with-front-matter.md
@@ -0,0 +1,9 @@
+---
+title: test title
+tags:
+  - post
+---
+
+# test title
+
+test content
--- test/markdown-file.test.js
+++ test/markdown-file.test.js
@@ -13,6 +13,20 @@ test('reads file information', async t => {
   t.snapshot(result.md)
 })
 
+test('reads file information with front matter', async t => {
+  const sourceFilePath = path.resolve(__dirname, 'fixtures', 'test-with-front-matter.md')
+  const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
+  const file = new MarkdownFile(sourceFilePath, absoluteWorkingDirectory)
+  t.truthy(file)
+  const result = await file.read()
+  t.deepEqual(result.attributes, {
+    tags: ['post'],
+    title: 'test title'
+  })
+  t.snapshot(result.html)
+  t.snapshot(result.md)
+})
+
 test('write output file', async t => {
   const sourceFilePath = path.resolve(__dirname, 'fixtures', 'test.md')
   const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
