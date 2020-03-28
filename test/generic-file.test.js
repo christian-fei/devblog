bd555c29b7bbe6bf1e2d4f39544e51a632aab2b9
Christian Fei <crifei93@gmail.com>
2020-03-28 09:55:02 +0100

    test unchanged files

--- test/generic-file.test.js
+++ test/generic-file.test.js
@@ -3,6 +3,9 @@ const GenericFile = require('../lib/files/generic-file')
 const fs = require('fs')
 const path = require('path')
 
+test.beforeEach(cleanup)
+test.after(cleanup)
+
 test('write output file', async t => {
   const sourceFilePath = path.resolve(__dirname, 'fixtures', 'image.png')
   const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
@@ -12,3 +15,21 @@ test('write output file', async t => {
   t.truthy(result.destinationFilePath.includes('/test/fixtures/_site/image.png'), result.destinationFilePath)
   t.true(fs.existsSync(result.destinationFilePath))
 })
+
+test('skips writes if unchanged', async t => {
+  const sourceFilePath = path.resolve(__dirname, 'fixtures', 'image.png')
+  const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
+  const file = new GenericFile(sourceFilePath, absoluteWorkingDirectory)
+
+  let result = await file.write()
+  t.falsy(result.unchanged)
+
+  result = await file.write()
+  t.truthy(result.unchanged)
+})
+
+function cleanup () {
+  try {
+    fs.rmdirSync(path.resolve(__dirname, 'fixtures', '_site'), { recursive: true })
+  } catch (err) {}
+}
--- test/markdown-file.test.js
+++ test/markdown-file.test.js
@@ -1,6 +1,10 @@
 const { serial: test } = require('ava')
 const MarkdownFile = require('../lib/files/markdown-file')
+const fs = require('fs')
 const path = require('path')
+
+test.beforeEach(cleanup)
+test.after(cleanup)
 
 test('reads file information', async t => {
   const sourceFilePath = path.resolve(__dirname, 'fixtures', 'test.md')
@@ -38,6 +42,18 @@ test('writes md output file', async t => {
   t.truthy(result.destinationFilePath.endsWith('/test/fixtures/_site/test.html'), result.destinationFilePath)
 })
 
+test('skips writes if unchanged', async t => {
+  const sourceFilePath = path.resolve(__dirname, 'fixtures', 'test.md')
+  const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
+  const file = new MarkdownFile(sourceFilePath, absoluteWorkingDirectory)
+
+  let result = await file.write()
+  t.falsy(result.unchanged)
+
+  result = await file.write()
+  t.truthy(result.unchanged)
+})
+
 test('writes njk output file', async t => {
   const sourceFilePath = path.resolve(__dirname, 'fixtures', 'test-nunjucks.njk')
   const absoluteWorkingDirectory = path.resolve(__dirname, 'fixtures')
@@ -49,3 +65,9 @@ test('writes njk output file', async t => {
   t.snapshot(result.html)
   t.truthy(result.destinationFilePath.endsWith('/test/fixtures/_site/test-nunjucks.html'), result.destinationFilePath)
 })
+
+function cleanup () {
+  try {
+    fs.rmdirSync(path.resolve(__dirname, 'fixtures', '_site'), { recursive: true })
+  } catch (err) {}
+}
